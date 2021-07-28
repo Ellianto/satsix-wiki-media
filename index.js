cf70c8e6a9dcee27aa7435c59675c1674b14c3ca
Ellianto <ellianto@10xers.co>
2021-07-29 00:43:31 +0700

    Initial Implementation of scraping hero unique

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
 // Allow the use of .env file
 require('dotenv').config();
 
+const scraper = require('./web-scraping/scraper');
+
 const discordBotToken = process.env.BOT_TOKEN;
+const prefix = '~';
+
+const BOT_COMMANDS = {
+  PING : 'ping',
+  SCRAPE : 'scrape',
+  TESTING : 'test',
+};
+
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login(discordBotToken);
+
+/*
+  TODO:
+  1) Use Web Scraper (puppetteer)
+  2) Params : Accept the wiki link and type (since diff wikis can use diff formats)
+  3) Scrape the essential text contents form the page
+  4) Layout them using HTML5 Canvas
+*/
+
+// Continuously listen to message
+client.on('message', async message => {
+  // If command doesn't start with prefix or sent by BOT, ignore
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  // Removes prefix and saves the command into a separate variable
+  // Also reads the remaining space separated string and save it into args
+  const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case BOT_COMMANDS.PING:
+      return message.channel.send('Pong.');
+    case BOT_COMMANDS.SCRAPE:
+      if (!args.length) {
+        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+      }
+
+      // TODO: Add check to make sure the link passed is of the correct domain
+      console.log(args);
+
+      const scrapedText = await scraper.scrape7DSWiki(args[0]);
+      return message.channel.send(scrapedText ? `Here's what I scraped:\n${scrapedText}` : `Failed to scrape anything from that link :(`);
+    case BOT_COMMANDS.TESTING:
+      const testing = await scraper.scrape7DSWiki('https://7dsgc.fandom.com/wiki/Red_Purgatory_Ban');
+      return message.channel.send(testing);
+    default:
+      break;
+  }
+});
